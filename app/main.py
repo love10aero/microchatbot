95b1ade43045d2325bf58e3acaa68dd61951dc88
Love <love.tr2014@gmail.com>
2024-05-08 10:47:43 +0200

    Working with mistral

--- app/main.py
+++ app/main.py
@@ -6,15 +6,17 @@ from langchain.chains import LLMChain
 
 app = FastAPI()
 
-# Load the LlamaCpp language model
+MODEL_PATH = ".\model\mistral-7b-instruct-v0.1.Q4_0.gguf"
+
+# Initialize the LlamaCpp language model with configured settings
 llm = LlamaCpp(
-    model_path=r"C:\Users\lovej\Documents\microchatbot\app\model\llama-2-7b-chat.Q4_K_M.gguf",
+    model_path=MODEL_PATH,
     n_gpu_layers=40,
     n_batch=512,
-    verbose=False
+    verbose=False  # Set to True for debugging
 )
 
-# Define the prompt template
+# Define the prompt template for structured queries
 template = """
 Question: {question}
 
@@ -29,9 +31,11 @@ class UserQuery(BaseModel):
 @app.post("/chat/")
 def chat_with_llama(query: UserQuery):
     try:
+        # Using the LangChain to run the query through the model
         answer = llm_chain.run(query.question)
         return {"answer": answer}
     except Exception as e:
+        # Better error handling to diagnose issues effectively
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/")
